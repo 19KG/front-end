baae6492b7e1d8e44950982345310c375a299f16
Thesharing <cyprestar@outlook.com>
2018-08-20 20:15:03 +0800

    Init commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <div class="logo">
+      <img src="../assets/logo.png">
+    </div>
+    <div class="search-bar">
+      <el-input
+        placeholder="请输入内容"
+        v-model="input23">
+        <i slot="prefix" class="el-input__icon el-icon-search"></i>
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+      <el-button v-for="item in userList">{{item}}</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      userList :[
+        '1',
+        '2',
+        '3',
+        '4',
+        '5'
+      ],
+      searchUserID: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/UserPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="userAvatar">
+      <img src="../assets/logo.png">
+    </div>
+    <div class="carousel-div">
+      <el-carousel :interval="4000" type="card" height="500px">
+        <el-carousel-item v-for="item in 5" :key="item">
+          <img v-bind:src="'/static/' + item.toString() + '.jpg'" class="carousel-img">
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "UserPage",
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style scoped>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+
+  .carousel-div {
+    width: 640px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .carousel-img {
+    height: 500px;
+  }
+</style>
